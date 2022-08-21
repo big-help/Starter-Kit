fee1c33894d4d44ca8ba9a87aabeb178d89b31c0
big-help <taewoo.leo.kim@gmail.com>
2022-08-21 14:43:18 +0900

    add random number generator

--- number-baseball/index.js
+++ number-baseball/index.js
@@ -1,1 +1,15 @@
-console.log("hello, vanilla.");
+
+function generateRandomNumber () {
+  const DIGIT_NUMBER = 5; 
+  const randomDigitNumber = Math.floor(Math.random() * Math.pow(10, DIGIT_NUMBER));
+
+  if (randomDigitNumber < Math.pow(10, DIGIT_NUMBER - 1)) {
+    return generateRandomNumber();
+  }
+
+  return randomDigitNumber;
+}
+
+
+
+

16ffc55ab2bc4f60be75d57d96e0d07b630fc861
big-help <taewoo.leo.kim@gmail.com>
2022-08-22 23:21:24 +0900

    add current date info

--- calendar/index.js
+++ calendar/index.js
@@ -1,1 +1,15 @@
-console.log("hello, vanilla.");
+const date = new Date();
+const current_date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+const current_day_box = document.getElementById("current-day");
+const current_date_box = document.getElementById("current-date");
+const arr_day = [
+  "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"
+];
+
+function editCurrentDateInfo () {
+  current_day_box.textContent = arr_day[current_date.getDay()];
+  current_date_box.textContent = current_date.getDate();
+}
+
+editCurrentDateInfo();

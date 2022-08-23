b91b72852ff819e634846b525d0dfea578a18351
big-help <taewoo.leo.kim@gmail.com>
2022-08-24 00:47:44 +0900

    add update calendar info

--- calendar/index.js
+++ calendar/index.js
@@ -1,9 +1,15 @@
+const MAX_DATE_NUM = 32;
+const MAX_WEEK_NUM = 6;
+const WEEKDAY = 7;
+
 const date = new Date();
 const current_date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+const first_date = new Date(date.getFullYear(), date.getMonth(), 1);
 
 const current_day_box = document.getElementById("current-day");
 const current_date_box = document.getElementById("current-date");
 const current_month_box = document.getElementsByClassName("current-month");
+const trElement = document.getElementsByTagName("tr");
 const arr_day = [
   "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"
 ];
@@ -11,10 +17,45 @@ const arr_month = [
   "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
 ];
 
-function editCurrentDateInfo () {
+function updateCurrentDateInfo () {
   current_day_box.textContent = arr_day[current_date.getDay()];
   current_date_box.textContent = current_date.getDate();
   current_month_box[0].children[0].textContent = `${arr_month[current_date.getMonth()]} ${current_date.getFullYear()}`;
 }
 
-editCurrentDateInfo();
+function setDayName () {
+  for (let i = 0; i < WEEKDAY; i++) {
+    trElement[0].children[i].textContent = arr_day[i];
+  }
+}
+
+function getMonthDateObject (year, month) {
+  let month_date_object = {};
+  for (let i = 1; i < MAX_DATE_NUM; i++) {
+    const date_info = new Date(year, month, i);
+
+    if (date_info.getMonth() === month)
+    month_date_object[date_info.getDate()] = date_info.getDay();
+  }
+
+  return month_date_object;
+}
+
+function updateCalendarDateInfo (year, month) {
+  let month_date_array = getMonthDateObject(year, month);
+  console.log(month_date_array);
+
+  for (const keys of month_date_array) {
+    
+    
+  }
+
+    for (let j = 1; j < MAX_WEEK_NUM; j++) {
+      trElement[j].children[date_info.getDay()].textContent = date_info.getDate();
+      if (trElement[j].children[date_info.getDay()] === 6) break;
+    }
+}
+
+updateCurrentDateInfo();
+setDayName();
+updateCalendarDateInfo(2022, 9);
